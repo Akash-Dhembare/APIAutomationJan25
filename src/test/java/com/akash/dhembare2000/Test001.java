d6e3a8d5a791c9d68fb7886e078422d889e68944
Akash <akash.dhembare777@gmail.com>
2025-01-28 18:17:35 +0530

    2 files are added

--- src/test/java/com/akash/dhembare2000/Test001.java
+++ src/test/java/com/akash/dhembare2000/Test001.java
@@ -1,2 +1,9 @@
-package com.akash.dhembare2000;public class Test001 {
+package com.akash.dhembare2000;
+
+import io.restassured.RestAssured;
+
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
